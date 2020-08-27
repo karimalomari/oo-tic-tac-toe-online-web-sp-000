9c7e6094434ec1ac9e90c3e2efa065cf0bf404c8
Karim Alomari <karim.alomari@gmail.com>
2020-08-27 20:23:55 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,34 @@ class TicTacToe
       end
   end
   
+  def valid_move?(index)
+    if (index < 0 || index > 8)
+      return false
+    elsif position_taken?(index)
+      return false
+    else
+      return true
+    end
+  end
+
+  def turn_count
+    @board.count{|token| token == "X" || token == "O"}
+  end
+
+  def current_player
+    turn_count % 2 == 0 ? "X" : "O"
+  end 
+
+  def turn
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+    if valid_move?(index)
+      move(index, current_player)
+      display_board
+    else
+     turn
+    end
+  end
+  
 end
